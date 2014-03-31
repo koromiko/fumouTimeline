0dd826a9bcf8956ea4079a16741bd364193b0dd7
Neo <koromiko1104@gmail.com>
2014-04-01 01:46:48 +0800

    new layout

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,6 +2,7 @@ console.log('\'Allo \'Allo!');
 
 
 var globalSpreadSheet = "https://docs.google.com/spreadsheet/pub?key=0Avv7Q0VS0N-wdFFudFhCNEQ4Wmo1WW1VbGxaZXhRNGc&output=html";
+var monthsArray = "一月,二月,三月,四月,五月,六月,七月,八月,九月,十月,十一月,十二月".split(",");
 
 $(document).ready(function() {
 
@@ -13,8 +14,29 @@ $(document).ready(function() {
 	function init() {
 	Tabletop.init( { key: public_spreadsheet_url,
 	                 callback: function( data ){ 
-	                 	console.log( data ); 
-	                 	renderWaterfallView( { "newsData":data } );
+	                 	
+	                 	// renderWaterfallView( { "newsData":data } );
+	                 	var dic = {};
+	                 	$.each( data.reverse() , function(index, val) {
+	                 		 /* iterate through array or object */
+	                 		 var eventDate = toDate(val.date);
+	                 		 var yearLabel = eventDate.getFullYear() + "." + monthsArray[ eventDate.getMonth() ];
+	                 		 var itemID = eventDate.getFullYear() + "-" + eventDate.getMonth();
+	                 		 var detailTime = eventDate.getFullYear() + "." + (eventDate.getMonth()+1) + "." + (eventDate.getDay()+1);
+	                 		 val.timeLabel = yearLabel;
+	                 		 val.detailTime = detailTime;
+	                 		 if(dic[itemID]==null){
+								dic[ itemID ] = [ val ];
+	                 		 }else{
+	                 		 	dic[ itemID ].push( val );	
+	                 		 }
+	                 		 
+	                 	});
+
+	                 	
+
+						renderWaterfallView( { "newsData":dic } );
+
 	                 },
 	                 simpleSheet: true } )
 	}
@@ -26,17 +48,27 @@ $(document).ready(function() {
 
 });
 
+function toDate(t){
+	var dd = new Date( Date.parse( t ) );
+	return dd;
+}
 
 function renderWaterfallView( data ){
 
+	
 	var $container = $('#waterfall-container');
-
 	loadTemplate( "item-template.html" , data , $container , function(){
-		// initialize
-		$container.masonry({
-		  
-		  itemSelector: '.item'
-		});	
+		
+
+		$.each( data['newsData'] , function(key, val) {
+			 /* iterate through array or object */
+			var $container = $('#waterfall-container-'+key);
+			
+			$container.masonry({
+				itemSelector: '.item'
+			});	
+		});
+		
 	} );
 
 	
